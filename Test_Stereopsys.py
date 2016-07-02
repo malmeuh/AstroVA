5882e3a0859ded2991ad49b07378f154fb3401f7
malmeuh <malmeuh@gmail.com>
2016-07-02 23:01:13 +0200

    Multi-thread
    two parallel threads, one grabbing images and the other accumulating them

--- Test_Stereopsys.py
+++ Test_Stereopsys.py
@@ -6,21 +6,30 @@ Created on Fri Feb 12 16:51:37 2016
 """
 
 from frameFusion2 import *
-from os import walk
+import frameGrabber  # Wrap the frame grabbing process
+import Queue
 
-f = []
-for (dirpath, dirnames, filenames) in walk('Images/Orion/'):
-    f.extend(filenames)
-    break
 
-# Initialize
-im = cv2.imread('Images/Orion/'+f[0])
-F = FrameFusion(im)
-F.show()
+def run():
 
-for i in range(1, len(f)):
-    filename = f[i]
-    im = Image.open('Images/'+filename)
-    a = np.asarray(im)
-    F.pile_up(a)
-    F.show()
+    # Initialize frame buffer for VA
+    f = FrameFusion()
+
+    # Get the inputs
+    frame_source = frameGrabber.PictsFile('Pics/')
+
+    pict_queue = Queue.Queue()
+    grabber = frame_source.populate(pict_queue, 1)
+
+    processing = f.process_pict(pict_queue, show=True)  # Blocking call
+
+
+    grabber.join()
+    processing.join()
+
+    frame_source.release()
+
+    print "Bye bye.."
+
+if __name__ == "__main__":
+    run()
--- frameFusion2.py
+++ frameFusion2.py
@@ -9,27 +9,56 @@ Class to combine different pictures on top of one another.
 
 import cv2
 import numpy as np
+import threading
+
+MAX_FRAME_NUMBER = 12
+
 
 class FrameFusion:
-
     # The constructor, on top of an initial frame
-    def __init__(self, frame_first, gamma = 1.0, motion_compensation = False):
+    def __init__(self):
         # Define settings
         self.n_fused_frames = 0
 
-        # Allocate buffer
-        self.last_frame = np.float32(frame_first)  # last frame
-        self.eq_frame = np.float32(frame_first)  # equalized frame
-        self.acc_frame = np.float32(frame_first)  # Sum of all frames
-        self.display_frame = np.float32(frame_first)  # Frame to display
+        self.list_frame = None
+
+    def _process_queue(self, pict_queue, show):
+        import Queue
+
+        while True:
+            try:
+                pic = pict_queue.get(block=True, timeout=5)
+                print "Got a new frame"
+
+                self.pile_up(pic)
+
+                if show:
+                    self.show()
+
+            except Queue.Empty:
+                print "No more frames"
+                break
+
+    def process_pict(self, pict_queue, show=False):
+        # Spawn a thread, which will read a picture
+        # Parallel threads (1)
+        for i in range(1):
+            t = threading.Thread(target=self._process_queue, args=(pict_queue, show))
+            t.start()
+
+        return t
 
     def show(self):
-
         # Show the current combined picture
         print "Showing frame {}".format(self.n_fused_frames)
+        frame_size = self.list_frame.shape
+
+        self.display_frame = np.ndarray([frame_size[0], frame_size[1], 3])
+        for i in range(3):
+            self.display_frame[:, :, i] = np.mean(self.list_frame[:, :, 1:self.n_fused_frames, i], 2)
 
         # Do all the resizing beforehand
-        frame_fusion_resize = cv2.resize(self.display_frame, (800,600))
+        frame_fusion_resize = cv2.resize(self.display_frame, (800, 600))
 
         cv2.namedWindow("FrameFusion")
         cv2.imshow("FrameFusion", np.uint8(frame_fusion_resize))
@@ -43,10 +72,15 @@ class FrameFusion:
         @param new_frame:
         @return: number of frames in the current pile
         """
+        if self.n_fused_frames == 0:
+            # Allocate buffer
+            frame_size = new_frame.shape
+            self.list_frame = np.ndarray([frame_size[0], frame_size[1], MAX_FRAME_NUMBER, 3])
+
 #        cv2.equalizeHist(new_frame, self.eq_frame)        
         
-        cv2.accumulate(new_frame, self.acc_frame)  # Second argument must be a float
-        cv2.normalize(self.acc_frame, self.display_frame, 0., 255., cv2.NORM_MINMAX)  # Normalize from 0 to 255
+        self.list_frame[:, :, self.n_fused_frames, :] = new_frame
+        # cv2.normalize(self.acc_frame, self.display_frame, 0., 255., cv2.NORM_MINMAX)  # Normalize from 0 to 255
         
         # Update and return
         self.n_fused_frames += 1
